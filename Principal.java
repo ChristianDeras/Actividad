acac13c7ab98883f379609211c0820c1c77113e0
ChristianDeras <71231223+ChristianDeras@users.noreply.github.com>
2020-09-14 16:05:12 -0600

    TErmine el objeto escuela

--- Principal.java
+++ Principal.java
@@ -3,6 +3,8 @@ public class Principal {
     public static void main(String Args[]) {
         //instanciando Escuela 
         Escuela c1 = new Escuela();
+		c1.nombre_escuela = "Mansion Guirola";
+		System.out.println(c1.nombre_escuela);
         //todo mi code
 		//ya termine todo
     }
