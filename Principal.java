6bfe2344201c4432a4efed05d49aa078b4c34df5
ChristianDeras <71231223+ChristianDeras@users.noreply.github.com>
2020-09-14 13:10:37 -0600

    Update Principal.java

--- Principal.java
+++ Principal.java
@@ -1,11 +1,7 @@
 import clases.*;
 public class Principal {
     public static void main(String Args[]) {
-        System.out.println("Hola acá editaremos todo");
-		//MarioZ5
-		System.out.println("Como usted diga don");
-        System.out.println("Enjoy it");
-        System.out.println("Acá con esto sabremos quien hizo que y como y a ala vez no nos toca unir todo ");
+
         //instanciando casa
         Casa c1 = new Casa();
 
