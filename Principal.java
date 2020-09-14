c0b1239ec49d1ce2bb2fc5f44de9361c165c3036
ChristianDeras <71231223+ChristianDeras@users.noreply.github.com>
2020-09-14 15:46:41 -0600

    Creacion de mi parte

--- Principal.java
+++ Principal.java
@@ -1,8 +1,8 @@
 import clases.*;
 public class Principal {
     public static void main(String Args[]) {
-        //instanciando casa
-        Casa c1 = new Casa();
-
+        //instanciando Escuela 
+        Escuela c1 = new Escuela();
+        //todo mi code
     }
 }
