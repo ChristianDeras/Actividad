e82f2e5eb1efa4216b9c65373affccfc8091cd09
MarioZ5 <71241583+MarioZ5@users.noreply.github.com>
2020-09-14 15:30:13 -0600

    Update Principal.java

--- Principal.java
+++ Principal.java
@@ -4,10 +4,14 @@ public class Principal {
         System.out.println("Hola acá editaremos todo");
 		//MarioZ5
 		System.out.println("Como usted diga don");
+<<<<<<< HEAD
+		System.out.println("Pooos creo que ahora todo va bien no ?");
+=======
         System.out.println("Enjoy it");
         System.out.println("Acá con esto sabremos quien hizo que y como y a ala vez no nos toca unir todo ");
         //instanciando casa
         Casa c1 = new Casa();
 
+>>>>>>> f2f208a6afb3c4d915cb7602d547764ffe337c57
     }
 }
