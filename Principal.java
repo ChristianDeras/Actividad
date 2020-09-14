e5fe93bab4d3087f07c2cc856ed325fefd41bf15
ChristianDeras <71231223+ChristianDeras@users.noreply.github.com>
2020-09-13 20:55:50 -0600

    INFORMACION IMPORTANTE

--- Principal.java
+++ Principal.java
@@ -3,5 +3,8 @@ public class Principal {
         System.out.println("Hola acá editaremos todo");
 		//MarioZ5
 		System.out.println("Como usted diga don");
+        System.out.println("Enjoy it");
+        System.out.println("Acá con esto sabremos quien hizo que y como y a ala vez no nos toca unir todo ")
+        
     }
 }
