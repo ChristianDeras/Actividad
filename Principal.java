2a46fd1a36f61608e9be72b2b47e04feabaade12
MarioZ5 <71241583+MarioZ5@users.noreply.github.com>
2020-09-13 20:44:53 -0600

    Update Principal.java

--- Principal.java
+++ Principal.java
@@ -1,6 +1,7 @@
 public class Principal {
     public static void main(String Args[]) {
         System.out.println("Hola acá editaremos todo");
+		//MarioZ5
 		System.out.println("Como usted diga don");
     }
 }
