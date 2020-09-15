c00be493a5a988af39557efc542d7cae55a2ec40
MarioZ5 <71241583+MarioZ5@users.noreply.github.com>
2020-09-15 14:54:46 -0600

    Update Familia.java

--- clases/Familia.java
+++ clases/Familia.java
@@ -1,7 +1,10 @@
 package clases;
 
 public class Familia {
-    public int num_familia;
-    public static void main(String[] args) {
-        
-    }
+    public int num_familiares;
+    public String nombres;
+    public String genero;
+    public String religion;
+    public String lugar_residencia;
+
+ }
